a7d6cc9a0b231f8d79be08429c519511620c291f
AayushGithub <66742440+AayushGithub@users.noreply.github.com>
2020-08-13 18:49:28 +0530

    Edits to Form Handling

--- src/components/forms/NewsletterForm.js
+++ src/components/forms/NewsletterForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import NetlifyForm from 'react-netlify-form';
 
 class NewsletterForm extends Component {
   state = {
@@ -14,14 +15,37 @@ class NewsletterForm extends Component {
     )
 
     return (
-      <form className={classNames}>
+      // <form className={classNames} method="POST" data-netlify="true">
+      //   <div className="control control-expanded">
+      //     <input className="input" type="email" name="email" placeholder="Enter Email ID&hellip;" />
+      //   </div>
+      //   <div className="control">
+      //     <button className="button button-primary button-block button-shadow" type="submit">{submit}</button>
+      //   </div>
+      // </form>
+      <NetlifyForm name='Contact Form'>
+      {({ loading, error, success }) => (
         <div className="control control-expanded">
-          <input className="input" type="email" name="email" placeholder="Enter Email ID&hellip;" />
+          {loading &&
+            <div>Loading...</div>
+          }
+          {error &&
+            <div>Your information was not sent. Please try again later.</div>
+          }
+          {success &&
+            <div>Thank you for contacting us!</div>
+          }
+          {!loading && !success &&
+            <div className="control control-expanded">
+                <input className="input" type="email" name="email" placeholder="Enter Email ID&hellip;" />
+              
+                 <button  className="control" className="button button-primary button-block button-shadow" type="submit">{submit}</button>
+               </div>
+             
+          }
         </div>
-        <div className="control">
-          <button className="button button-primary button-block button-shadow" type="submit">{submit}</button>
-        </div>
-      </form>
+      )}
+    </NetlifyForm>
     )
   }
 }
